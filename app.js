1ac6e1abf39974a178c5995f278831bf0a426adb
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-03-24 19:55:29 +0500

    Database

--- app.js
+++ app.js
@@ -6,22 +6,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//        Connection Start
+
 mongoose.connect('mongodb+srv://Sadaan:dbdb1@cluster0.qq2alg8.mongodb.net/Sadaan_DataBase')
-mongoose.connection.on('error',(error)=>{
+
+mongoose.connection.on('error', (error) => {
     console.log(error)
 })
-mongoose.connection.once('connected',()=>{
+mongoose.connection.once('connected', () => {
     console.log('Database Connected');
 })
 
+//        Connection End
 
+// Schema Start
+const userSchema = new mongoose.Schema({
+    userName : String,
+    password : String
+})
+// Schema End
 
+// Model Start
+const userModel = new mongoose.model('myUsers',userSchema);
+// Model End 
 
+app.get('/', (req, res) => {
+    res.status(200).json({msg:'Server has been Started'});    
 
-
+})
+app.get('/create',(req,res)=>{
+    const myUser = new userModel({
+        userName:'Sadaan',
+        password:'abc123',
+    })
+    console.log(myUser);
+    res.send('It is Create')
+})
 
 
 let port = process.env.PORT || 5000;
 app.listen(port,()=>{
-    console.log(`Server is started at port ${port}`)
+    console.log(`Server is started at port ${ port }`)
 })

005f0c7621be334be72dbbea117484a746903a28
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-03-31 00:09:30 +0500

    DB

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose')
+
+require('dotenv').config();
+console.log(process.env.DB_LINK)
+
 const app = express();
 
 app.use(cors());
@@ -8,7 +12,7 @@ app.use(express.json());
 
 //        Connection Start
 
-mongoose.connect('mongodb+srv://Sadaan:dbdb1@cluster0.qq2alg8.mongodb.net/Sadaan_DataBase')
+mongoose.connect(process.env.DB_LINK)
 
 mongoose.connection.on('error', (error) => {
     console.log(error)
@@ -21,8 +25,15 @@ mongoose.connection.once('connected', () => {
 
 //            Schema Start
 const userSchema = new mongoose.Schema({
-    userName: String,
-    password: String
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
 })
 const userProducts = new mongoose.Schema({
     name: String,
@@ -42,15 +53,7 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Server has been Started' });
 
 })
-app.post('/create_user', async (req, res) => {
-    const myUser = new userModel({
-        userName: req.body.userName,
-        password: req.body.password,
-    })
-    const output = await myUser.save()
-    console.log(output)
-    res.send(output)
-})
+
 
 app.post('/create_product', async (req, res) => {
     const myProducts = new productModel({
@@ -61,8 +64,66 @@ app.post('/create_product', async (req, res) => {
     })
     const output = await myProducts.save();
     console.log(output)
-    res.send(output)
+    res.json(output)
 })
+app.get('/get_product', async (req, res) => {
+    const output = await productModel.find({ price: 25 })
+    console.log(output);
+    res.status(200).send(output)
+})
+// app.get('/update_product', async (req, res) => {
+//     const output = await productModel.find({ price: 25 })
+//     console.log(typeof (output));
+//     // let new_output = output.map((item)=>{return(item.rating = 5.0)})
+//     // for (let i = 0; i < output.length; i++) {
+//     //     output[i].rating == 9.8;
+//     // }
+//     // let new_output = output[output.length].review;
+//     output.rating = 5.8;
+//     console.log(output)
+//     res.status(200).json({ output, msg: 'Update Product' });
+// })
+app.get('/update_product',async (req, res) => {
+    await productModel.updateMany({}, { $set: { rating: 6.7 } })
+        .then(result => {
+            console.log(result);
+            res.status(200).send('Updating Product');
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).send('Error Updating Product');
+        });
+});
+
+
+app.post('/create_user', async (req, res) => {
+    const myUser = new userModel({
+        userName: req.body.userName,
+        password: req.body.password,
+    })
+    try {
+        const output = await myUser.save()
+        console.log(output)
+        res.status(200).json(output)
+    } catch {
+        res.status(400).send("Error Occurred")
+    }
+})
+app.get('/get_user', async (req, res) => {
+    const output = await userModel.findOne({ userName: 'Sadaan' })
+    // let newOutput = output.filter((item) => { return (item.userName === 'Habib') })
+    console.log(output)
+    res.status(200).json(output)
+})
+app.get('/update_user', async (req, res) => {
+    const output = await userModel.findOne({ userName: req.body.userName })
+    output.password = 'Mera password';
+    let new_output = await output.save();
+    console.log(output);
+    res.status(200).json(new_output);
+})
+
+
 
 
 let port = process.env.PORT || 5000;

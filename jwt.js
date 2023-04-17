3472f3d07cf11aa3f2317e7b81469b063aa3ca31
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-04-18 00:11:44 +0500

    Secure

--- jwt.js
+++ jwt.js
@@ -1,9 +1,9 @@
 const express = require('express');
 var cors = require('cors');
-// const mongoose = require('mongoose');
-// const jwt = require('jsonwebtoken');
-// require('dotenv').config();
-
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+console.log(process.env.DB_LINK)
 const app = express();
 
 app.use(cors());
@@ -12,15 +12,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
     try {
         res.status(200).send('My Server !')
-    } catch {
-        res.status(404).send("Error Occurred")
-    }
-})
-
-app.post('/shirtProducts', validation, (req, res) => {
-    try {
-        console.log(req.headers)
-        res.status(200).send('Response of Shirts !')
     } catch {
         res.status(404).send("Error Occurred")
     }
@@ -38,6 +29,20 @@ app.post('/signUp', (req, res) => {
         res.status(404).send('Not Found');
     }
 
+})
+
+app.post('/login',(req,res)=>{
+    console.log(req.body)
+    try{
+        if(req.body.userName && req.body.password){
+            // if(req.body.userName = ) 'Condition likhni hai jis ke liye mongo db lagani pare gi
+            res.status(200).send('You have Logged in Successfully')
+        }else{
+            res.status(404).send('Data Not Recieved')
+        }
+    }catch{
+        res.status(404).send('Error Occurred')
+    }
 })
 
 app.get('/shirtProducts', validation, (req, res) => {
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const test = require('./test2')
 test.funcFive()
 require('dotenv').config();
-console.log(process.env.DB_LINK)
+console.log(process.env.SECRET_KEY)
 
 const jwt = require('jsonwebtoken');
 
@@ -10,7 +10,7 @@ const jwt = require('jsonwebtoken');
 // console.log(token)
 
 // const secret_key = process.env.DB_LINK;
-const secret_key ='123321';
+const secret_key =process.env.SECRET_KEY;
 
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNhZGFhbiIsImlhdCI6MTY4MTE1MDczNn0.E5-1X-ZcBkQVDIDmHQ-Zna-e5HVKtUhh0jWFHSz81lo'
 const decoded = jwt.verify(token, secret_key)
